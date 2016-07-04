525c62a8314ce7a318b828ccc5fa5570630fbfdc
Roshan <divmeh@hotmail.com>
2016-07-04 23:22:33 +0530

    Time limit removed

--- resources/views/emails/forgot_password.blade.php
+++ resources/views/emails/forgot_password.blade.php
@@ -7,7 +7,6 @@ To complete the process, click the link below. <br/>
 <a href="{{$url}}">Click here to reset your password.</a>
 <br/><br/>
 If you did not ask to change your password, then you can ignore this email and your password will not be changed.
-The link above will remain active for 1 hour.
 <br/><br/>
 The Lineup Beast<br/><br/>
 <a href="{{config('app.url')}}">{{config('app.url')}}</a>
