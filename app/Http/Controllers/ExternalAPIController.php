e698a36d27695dc074a8782c7137ea12606672df
Roshan <divmeh@hotmail.com>
2016-07-09 21:02:46 +0530

    Re wrote all methods

--- app/Http/Controllers/ExternalAPIController.php
+++ app/Http/Controllers/ExternalAPIController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use App\Http\Requests;
 use DB;
 use Illuminate\Support\Facades\Log;
@@ -11,72 +10,144 @@ use Illuminate\Support\Facades\Log;
 class ExternalAPIController extends Controller
 {
 
-    public function index(){
+    const API_URL                                       =   'https://api.fantasydata.net/mlb/v2/json';
+    const URL_SEPARATOR                                 =   PATH_SEPARATOR;
 
-        // APIs URLs TO IMPORT SPORTS DATA - FANTASY
-        $urls = [
-            'mlb_team'          => 'https://api.fantasydata.net/mlb/v2/json/teams',
-            'mlb_news'          => 'https://api.fantasydata.net/mlb/v2/json/News',
-            'mlb_player'        => 'https://api.fantasydata.net/mlb/v2/json/Players',
-            'mlb_stadium'       => 'https://api.fantasydata.net/mlb/v2/json/Stadiums',
-            'mlb_schedual'      => 'https://api.fantasydata.net/mlb/v2/json/Games/2016',
-            'mlb_playerseason'  => 'https://api.fantasydata.net/mlb/v2/json/PlayerSeasonStats/2016',
-        ];
+    //-- Endpoints
+    const MLB_TEAMS                                     =   'teams';
+    const MLB_ARE_GAME_IN_PROGRESS                      =   'AreAnyGamesInProgress';
+    const MLB_BATTERS_VS_BATTERS_STATS                  =   'HitterVsPitcher';
+    const MLB_BOX_SCORES                                =   'BoxScore';
+    const MLB_BOX_SCORES_BY_DATE                        =   'BoxScores';
+    const MLB_BOX_SCORES_BY_DATE_DELTA                  =   'BoxScoresDelta';
+    const MLB_PLAYER_GAMES_STAT_BY_DATE                 =   'PlayerGameStatsByDate';
+    const MLB_PLAYER_GAMES_STAT_BY_PLAYER               =   'PlayerGameStatsByPlayer';
+    const MLB_PLAYER_SEASON_SPLIT_STATS                 =   'PlayerSeasonSplitStats';
+    const MLB_PLAYER_SEASON_STATS                       =   'PlayerSeasonStats';
+    const MLB_PROJECTED_PLAYER_GAME_STATS_BY_DATE       =   'PlayerGameProjectionStatsByDate';
+    const MLB_PROJECTED_PLAYER_GAME_STATS_BY_PLAYER     =   'PlayerGameProjectionStatsByPlayer';
+    const MLB_TEAM_GAME_STAT_BY_DATE                    =   'TeamGameStatsByDate';
+    const MLB_TEAM_SEASON_STATS                         =   'TeamSeasonStats';
+    const MLB_NEWS                                      =   'News';
+    const MLB_PLAYER                                    =   'Players';
+    const MLB_STADIUM                                   =   'Stadiums';
+    const MLB_SCHEDULE                                  =   'Games/2016';
 
-        foreach ($urls as $table => $url) {
 
-        $data = json_decode($this->curl($url), true);
+    public function teams(){
+       return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_team');
+    }
 
-        $values_arr = [];
+    public function AreAnyGamesInProgress(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_are_any_games_in_progress');
+    }
 
-            foreach($data as $k=>$value) {
+    public function HitterVsPitcher(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_ARE_GAME_IN_PROGRESS, 'mlb_hitters_vs_pitcher');
+    }
 
-                    // REMOVE EXTRA KEY
-                if(array_key_exists('Key', $value))
+    public function BoxScore(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_box_score');
+    }
+
+    public function BoxScoresByDelta(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_box_scores_by_delta');
+    }
+
+    public function PlayerGameStatsByDate(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_player_game_stats_by_date');
+    }
+
+    public function PlayerGameStatsByPlayer(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_player_game_stats_by_player');
+    }
+
+    public function PlayerSeasonSplitStats(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_player_season_split_stats');
+    }
+
+    public function PlayerSeasonStats(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_player_season_stats');
+    }
+
+    public function PlayerGameProjectionStatsByDate(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_player_game_projection_stats_by_date');
+    }
+
+    public function PlayerGameProjectionStatsByPlayer(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_player_game_projection_stats_by_player');
+    }
+
+    public function TeamGameStatsByDate(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_team_game_stats_by_date');
+    }
+
+    public function News(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_news');
+    }
+
+    public function Stadiums(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_stadium');
+    }
+
+    public function Schedules(){
+        return $this->processData(self::API_URL . self::URL_SEPARATOR . self::MLB_TEAMS, 'mlb_schedual');
+    }
+
+    public function processData($url,$table){
+
+        $data   =   $this->curl($url);
+
+        if(!json_decode($data)){
+            Log::error('Failed to pull data for API URL : ' . $url);
+            return false;
+        }
+
+        return $this->addToDB($table,$data);
+    }
+
+    public function addToDB($table,$data){
+
+        if(empty($data) || !is_array($data) || !count($data)){
+            return false;
+        }
+
+        foreach($data as $k=>$value) {
+
+            // REMOVE EXTRA KEY
+            if(array_key_exists('Key', $value))
 
                 unset($value['Key']);
 
-                $value  = $this->filterArrayForTable($value,$table);
+            $value  = $this->filterArrayForTable($value,$table);
 
-                if(array_key_exists('RunnerOnFirst',$value)) {
-                   unset($value['RunnerOnFirst']);
-                }
+            $columns_str = implode(",", array_keys($value));
 
-                if(array_key_exists('RunnerOnSecond',$value)){
-                    unset($value['RunnerOnSecond']);
-                }
+            $value = preg_replace("/[^a-zA-Z 0-9]+/", "", $value );
+            $values_str = implode("','", $value);
 
-                if(array_key_exists('RunnerOnThird',$value)){
-                    unset($value['RunnerOnThird']);
-                }
-
-                $columns_str = implode(",", array_keys($value));
-
-                $value = preg_replace("/[^a-zA-Z 0-9]+/", "", $value );
-                $values_str = implode("','", $value);
-
-                $values_arr[] = "('".$values_str."')";
-            }
+            $values_arr[] = "('".$values_str."')";
+        }
 
         $final_values = implode(",", $values_arr);
 
-            try{
-                // TRUNCATE EXISTING DATA
-                DB::statement("TRUNCATE TABLE ".$table);
+        try{
+            // TRUNCATE EXISTING DATA
+            DB::statement("TRUNCATE TABLE ".$table);
 
-                // INSERT NEW VALUES
-                $insert_query_string = DB::insert("INSERT INTO ".$table." (".$columns_str.") VALUES ".$final_values);
+            // INSERT NEW VALUES
+            $insert_query_string = DB::insert("INSERT INTO ".$table." (".$columns_str.") VALUES ".$final_values);
 
-                if($insert_query_string) {
-                    Log::info("Records inserted successfully for table " . $table . " Date" . Carbon::now()->toDayDateTimeString());
-                }
-            }catch (Exception $e){
-                Log::info("Failed to add record for table " . $table . " Date" . Carbon::now()->toDayDateTimeString());
+            if($insert_query_string) {
+                Log::info("Records inserted successfully for table " . $table . " Date" . Carbon::now()->toDayDateTimeString());
             }
+        }catch (Exception $e){
+            Log::info("Failed to add record for table " . $table . " Date" . Carbon::now()->toDayDateTimeString());
         }
     }
 
     public function curl($send_url) {
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Ocp-Apim-Subscription-Key: 43ed5d29a9b5413e8254a38b55ddf73c' ));
         curl_setopt($ch, CURLOPT_URL, $send_url);
@@ -85,11 +156,12 @@ class ExternalAPIController extends Controller
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_VERBOSE, 0);
         $result = curl_exec($ch);
+
         if (curl_errno($ch)) {
             $error = curl_error($ch);
             curl_close($ch);
 
-            throw new Exception("Failed retrieving  '" . $this->send_url . "' because of ' " . $error . "'.");
+            Log::error("Failed retrieving  '" . $send_url . "' because of ' " . $error . "'.");
         }
         return $result;
     }
@@ -98,13 +170,12 @@ class ExternalAPIController extends Controller
 
         foreach($row as $key=>$val){
             $q = DB::select("SELECT * FROM information_schema.COLUMNS WHERE
-                 TABLE_SCHEMA = '".env('DB_DATABASE')."' AND
-                  TABLE_NAME = '$table' AND COLUMN_NAME = '$key'");
+                    TABLE_SCHEMA = '".env('DB_DATABASE')."' AND
+                    TABLE_NAME = '$table' AND COLUMN_NAME = '$key'");
             if(!$q){
                 unset($row[$key]);
             }
         }
         return $row;
-
     }
 }
